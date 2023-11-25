415d48f9e2ff07ca49ac23f78fbf131f06a9103f
Martin Quick <mquick@oxford-analytica.com>
2023-11-25 19:54:30 +0000

    add test endpoint

--- app/src/api/posts.py
+++ app/src/api/posts.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
+import json
+from typing import Dict, List
 
 app = FastAPI()
+
+data = json.load(open("./data/posts.json", "r"))
+
+
+@app.get("/")
+def get_posts() -> Dict[str, List]:
+    return {"posts": data}
