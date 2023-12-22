a5a7445f3054f837f87bcf339cdd766357df4dbf
Martin Quick <mquick@oxford-analytica.com>
2023-12-22 22:16:51 +0000

    add function to set visible post content

--- app/src/components/Post.js
+++ app/src/components/Post.js
@@ -1,10 +1,20 @@
+import { Link } from "react-router-dom";
+
 function Post(props) {
+
+    function VisibleContent(content) {
+        const suffix = content.length > 100 ? "..." : ""
+        return content.slice(0, 100) + suffix
+    }
 
     return (
         <div>
-            <h2>{props.title}</h2>
-            <time>{props.date}</time>
-            <p>{props.content.slice(0, 100) + "..."}</p>
+            <div className="container border">
+                <h2><Link to={`/posts/${props.id}`}>{props.title}</Link></h2>
+                <time>{props.date}</time>
+                <p>{VisibleContent(props.content)}</p>
+            </div>
+            <br></br>
         </div>
     );
 }
