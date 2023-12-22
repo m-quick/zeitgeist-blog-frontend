31e1ac2c32e83fe3304ad5a168de720dac2a778a
Martin Quick <mquick@oxford-analytica.com>
2023-12-22 20:54:05 +0000

    move post iteration to Home page

--- app/src/App.js
+++ app/src/App.js
@@ -1,36 +1,12 @@
 import './App.css';
-import React, { useState, useEffect } from "react";
-import api from "./api.js";
-import Post from './Post.js';
-import Navbar from './Navbar.js';
+import Navbar from './components/Navbar.js';
 import Home from './pages/Home.js'
 import CreatePost from './pages/CreatePost.js'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 
-  const [posts, setPosts] = useState([]);
-
-  const fetchPosts = async () => {
-    const response = await api.get("/")
-    setPosts(response.data.posts)
-  };
-
-  useEffect(() => {
-    fetchPosts()
-  }, []);
-
   return (
-    // <div className="App">
-    //   <Navbar />
-    //   {posts.map((post, index) => (
-    //     <Post
-    //       title={post.title}
-    //       date={post.pubDate}
-    //       content={post.content}
-    //     />
-    //   ))}
-    // </div>
     <div>
       <Router>
         <Navbar />
--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,7 +1,29 @@
+import Post from '../components/Post.js';
+import React, { useState, useEffect } from "react";
+import api from "../api.js";
+
 function Home() {
+
+    const [posts, setPosts] = useState([]);
+
+    const fetchPosts = async () => {
+        const response = await api.get("/")
+        setPosts(response.data.posts)
+    };
+
+    useEffect(() => {
+        fetchPosts()
+    }, []);
+
     return (
         <div>
-            Home
+            {posts.map((post, index) => (
+                <Post
+                    title={post.title}
+                    date={post.pubDate}
+                    content={post.content}
+                />
+            ))}
         </div>
     );
 }
