ebfd4a1068c1640bf85245008503afde07670b8f
Martin Quick <mquick@oxford-analytica.com>
2023-12-22 20:45:06 +0000

    add bootstrap and alert success message when new post is added

--- app/src/pages/CreatePost.js
+++ app/src/pages/CreatePost.js
@@ -1,15 +1,28 @@
 import React, { useState } from "react";
+import api from "../api.js"
+
 
 function CreatePost() {
 
     const [title, setTitle] = useState("")
     const [post_text, setPostText] = useState("")
+    const [successMessage, setSuccessMessage] = useState(false);
 
-    console.log(title, post_text)
+    const addPost = async () => {
+        try {
+            const response = await api.post("create_post", { "title": title, "content": post_text });
+            if (response.data === true) {
+                setSuccessMessage(true);
+            }
+        } catch (error) {
+            console.error("Error adding post:", error);
+        }
+    };
 
     return (
         <div>
             <h1>Create a post</h1>
+            {successMessage && <div class="alert alert-success" role="alert">Post added!</div>}
             <div>
                 <label>Title</label>
                 <input placeholder="Title" onChange={(event) => { setTitle(event.target.value) }}></input>
@@ -18,7 +31,7 @@ function CreatePost() {
                 <label>Post</label>
                 <textarea placeholder="Post" onChange={(event) => { setPostText(event.target.value) }}></textarea>
             </div>
-            <button>Submit</button>
+            <button onClick={addPost}>Submit</button>
         </div >
     );
 }
