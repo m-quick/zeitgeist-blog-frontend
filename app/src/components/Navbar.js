88f7ad2fc5eaaeedbfa6a6a3043c756eb10b4d77
Martin Quick <martinquick7@gmail.com>
2024-01-21 19:39:56 +0000

    wrap navbar contents in div container and delete custom css for navbar

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -3,16 +3,23 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <nav className="nav">
-            <Link to="/" className="blog-title">Zeitgeist</Link>
-            <ul>
-                <li>
-                    <Link to="/create_post" >New post</Link>
-                </li>
-                <li>
-                    <Link to="/about" >About</Link>
-                </li>
-            </ul>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
+            <div class="container">
+                <h2>
+                    <Link to="/" class="navbar-brand fs-4 fw-normal">
+                        {/* <img src="favicon.png" alt="" /> */}
+                        Zeitgeist
+                    </Link>
+                </h2>
+                <ul class="navbar-nav mr-auto fs-6">
+                    <li class="nav-item">
+                        <Link to="/create_post" class="nav-link">New post</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/about" class="nav-link">About</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>);
 }
 
