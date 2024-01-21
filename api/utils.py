47ac88a6307e2920feb9f4e331dc901c0f22b5e9
Martin Quick <martinquick7@gmail.com>
2024-01-21 20:10:52 +0000

    specify date output format in api

--- api/utils.py
+++ api/utils.py
@@ -4,7 +4,7 @@ from typing import List, Dict
 
 def sort_posts(posts: List[Dict[str, str]]) -> List[Dict[str, str]]:
     posts_df = pd.DataFrame(posts)
-    posts_df["pubDate"] = pd.to_datetime(posts_df["pubDate"])
+    posts_df["pubDate"] = pd.to_datetime(posts_df["pubDate"]).dt.strftime("%d %B %Y")
     posts_df_sorted = posts_df.sort_values("pubDate", ascending=False)
     posts_df_sorted["pubDate"] = posts_df_sorted["pubDate"].astype(str)
     posts_sorted = posts_df_sorted.to_dict("records")
