55bc6bd38387dae5b8dc8cb5972de3c36421d77b
Martin Quick <martinquick7@gmail.com>
2024-03-20 22:24:30 +0000

    add sidebar next to blog posts in sie by side view

--- app/src/pages/LandingPage.js
+++ app/src/pages/LandingPage.js
@@ -1,20 +1,31 @@
 import Post from "../components/Post";
+import Sidebar from "../components/Sidebar";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function LandingPage(props) {
     return (
-        <div>
-            <div className="container rounded">
-                <h1 class="display-1 fs-2 fw-normal my-3">Blog posts</h1>
-            </div>
-            {props.posts.map((post, index) => (
-                <Post
-                    id={post.id}
-                    title={post.title}
-                    date={post.pubDate}
-                    content={post.content}
-                />
-            ))}
-        </div>
+        <Container>
+            <Row>
+                <Col>
+                    <Sidebar />
+                </Col>
+                <Col xs={9}>
+                    <div className="container rounded">
+                        <h1 class="display-1 fs-2 fw-normal my-3">Blog posts</h1>
+                    </div>
+                    {props.posts.map((post, index) => (
+                        <Post
+                            id={post.id}
+                            title={post.title}
+                            date={post.pubDate}
+                            content={post.content}
+                        />
+                    ))}
+                </Col>
+            </Row>
+        </Container >
     )
 }
 
